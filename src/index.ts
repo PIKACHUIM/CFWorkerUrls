f121da8fff3d36bf066380625ed7b4a19e774e0e
Pikachu Ren <40362270+PIKACHUIM@users.noreply.github.com>
2025-08-04 20:08:38 +0800

    mod(platform): edgeone

--- src/index.ts
+++ src/index.ts
@@ -461,51 +461,51 @@ interface Dict {
     [key: string]: string;
 }
 
-
-app.fire()
+export default app
+// app.fire()
 // 定时任务 ############################################################################################################
-// export default app
-export default {
-    async fetch(request: Request, env: Bindings, ctx: ExecutionContext) {
-        return app.fetch(request, env, ctx);
-    },
-    async scheduled(controller: ScheduledController, env: Bindings, ctx: ExecutionContext) {
-        console.log('Cron job processed');
-        try {
-            console.log(controller, ctx)
-            const keys = await env.DATABASE.list();
-            for (const key of keys.keys) {
-                const value = await env.DATABASE.get(key.name);
-                console.log(key.name, value);
-                if (value) {
-                    const detail = JSON.parse(value);
-                    const timers = detail.timers;
-                    if (!timers) {
-                        await env.DATABASE.delete(key.name);
-                        console.log("Delete Invalid", key.name, value);
-                        continue;
-                    }
-                    // 将 timers 转换为日期对象
-                    const oldDate = new Date(Number(timers));
-                    const nowDate = new Date();
-                    // 计算时间差（天数）
-                    const diffTime: number = nowDate.getTime() - oldDate.getTime();
-                    const expsTime: number = Number(detail["expire"]) * 1000 * 60 * 60 * 24
-                    console.log(
-                        "\nName Records: " + key.name,
-                        "\nLast Updated: " + oldDate,
-                        "\nCurrent Time: " + nowDate,
-                        "\nWaiting Hour: " + Math.ceil(diffTime / 1000 / 3600),
-                        "\nConfigs Hour: " + Math.ceil(diffTime / 1000 / 3600));
-                    // 如果时间差大于等于 expireDays，则删除该键值对
-                    if (diffTime >= expsTime) {
-                        await env.DATABASE.delete(key.name);
-                        console.log(`Deleted key: ${key.name}`);
-                    }
-                }
-            }
-        } catch (error) {
-            console.error('Error processing cron job:', error);
-        }
-    },
-};
+//
+// export default {
+//     async fetch(request: Request, env: Bindings, ctx: ExecutionContext) {
+//         return app.fetch(request, env, ctx);
+//     },
+//     async scheduled(controller: ScheduledController, env: Bindings, ctx: ExecutionContext) {
+//         console.log('Cron job processed');
+//         try {
+//             console.log(controller, ctx)
+//             const keys = await env.DATABASE.list();
+//             for (const key of keys.keys) {
+//                 const value = await env.DATABASE.get(key.name);
+//                 console.log(key.name, value);
+//                 if (value) {
+//                     const detail = JSON.parse(value);
+//                     const timers = detail.timers;
+//                     if (!timers) {
+//                         await env.DATABASE.delete(key.name);
+//                         console.log("Delete Invalid", key.name, value);
+//                         continue;
+//                     }
+//                     // 将 timers 转换为日期对象
+//                     const oldDate = new Date(Number(timers));
+//                     const nowDate = new Date();
+//                     // 计算时间差（天数）
+//                     const diffTime: number = nowDate.getTime() - oldDate.getTime();
+//                     const expsTime: number = Number(detail["expire"]) * 1000 * 60 * 60 * 24
+//                     console.log(
+//                         "\nName Records: " + key.name,
+//                         "\nLast Updated: " + oldDate,
+//                         "\nCurrent Time: " + nowDate,
+//                         "\nWaiting Hour: " + Math.ceil(diffTime / 1000 / 3600),
+//                         "\nConfigs Hour: " + Math.ceil(diffTime / 1000 / 3600));
+//                     // 如果时间差大于等于 expireDays，则删除该键值对
+//                     if (diffTime >= expsTime) {
+//                         await env.DATABASE.delete(key.name);
+//                         console.log(`Deleted key: ${key.name}`);
+//                     }
+//                 }
+//             }
+//         } catch (error) {
+//             console.error('Error processing cron job:', error);
+//         }
+//     },
+// };
